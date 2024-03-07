2e4c8aa46721068f6d10d45f71e9e48469674ce3
Ian <ian@g.ucla.edu>
2024-03-06 23:58:39 -0800

    fix: changed default api base url back to openai's official url

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -138,7 +138,7 @@ async function fetchUsage() {
 
   const API_BASE_URL = isNotEmptyString(OPENAI_API_BASE_URL)
     ? OPENAI_API_BASE_URL
-    : 'https://dataana.top'
+    : 'https://https://api.openai.com/'
 
   const [startDate, endDate] = formatDate()
 
